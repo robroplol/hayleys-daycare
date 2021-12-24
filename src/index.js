18b90f25e57645dd3d98d360f3fa79e6a2cdfccc
robroplol <robrop92@gmail.com>
2021-12-24 12:46:27 -0500

    changes

--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
 <<<<<<< HEAD
+<<<<<<< HEAD
 console.log("idk test");
 =======
+=======
+>>>>>>> develop
 var $ = require("jquery");
 var slick = require("slick-carousel");
 
 $(document).ready(function () {
   $("#img-carousel").slick({});
 });
+<<<<<<< HEAD
+>>>>>>> develop
+=======
 >>>>>>> develop
