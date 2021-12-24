2179207915cb157faeb9345649053dc112fa6bd1
robroplol <robrop92@gmail.com>
2021-12-24 12:45:14 -0500

    test

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
+<<<<<<< HEAD
 console.log("idk test");
+=======
+var $ = require("jquery");
+var slick = require("slick-carousel");
+
+$(document).ready(function () {
+  $("#img-carousel").slick({});
+});
+>>>>>>> develop
